82229693445fc3f1732dca5dc39540813b6034be
bluelovers <codelovers@users.sourceforge.net>
2011-07-02 14:30:09 +0800

    get all by adblock hide elem

--- jsco.adblock.detect/jsco.adblock.detect.js
+++ jsco.adblock.detect/jsco.adblock.detect.js
@@ -1,3 +1,8 @@
 (function($){
 
+	// get all by adblock hide elem
+	$('*').filter(function(){
+		return jQuery(this).css('-moz-binding').match(/url\s*\(\s*"about:abp-elemhidehit\?/is);
+	});
+
 })(jQuery);
