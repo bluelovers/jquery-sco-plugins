9a8543ced4c511b330af8b9efe3911b19ca62daf
bluelovers <codelovers@users.sourceforge.net>
2011-07-02 14:37:12 +0800

    add check chrome://global/content/bindings/general.xml#foobarbazdummy

--- jsco.adblock.detect/jsco.adblock.detect.js
+++ jsco.adblock.detect/jsco.adblock.detect.js
@@ -1,8 +1,8 @@
 (function($){
 
 	// get all by adblock hide elem
-	$('*').filter(function(){
-		return jQuery(this).css('-moz-binding').match(/url\s*\(\s*"about:abp-elemhidehit\?/is);
+	jQuery('*').filter(function(){
+		return jQuery(this).css('-moz-binding').match(/url\s*\(\s*"(about:abp-elemhidehit|chrome:\/\/global\/content\/bindings\/general\.xml#foobarbazdummy)/i);
 	});
 
 })(jQuery);
