d5ab7ae516a91e4a98a8fe84bf37ad39de14d190
bluelovers <codelovers@users.sourceforge.net>
2011-07-02 14:41:39 +0800

    split code

--- jsco.adblock.detect/jsco.adblock.detect.js
+++ jsco.adblock.detect/jsco.adblock.detect.js
@@ -2,7 +2,11 @@
 
 	// get all by adblock hide elem
 	jQuery('*').filter(function(){
-		return jQuery(this).css('-moz-binding').match(/url\s*\(\s*"(about:abp-elemhidehit|chrome:\/\/global\/content\/bindings\/general\.xml#foobarbazdummy)/i);
+		return _ishide(this);
 	});
+
+	function _ishide(elem) {
+		return jQuery(elem).css('-moz-binding').match(/url\s*\(\s*[\"\']?(about:abp-elemhidehit|chrome:\/\/global\/content\/bindings\/general\.xml#foobarbazdummy)/i);
+	}
 
 })(jQuery);
