1aa9517814a37258cff017b57621371b5058f78f
bluelovers <codelovers@users.sourceforge.net>
2011-07-02 14:57:01 +0800

    rewrite to jquery

--- jsco.adblock.detect/jsco.adblock.detect.js
+++ jsco.adblock.detect/jsco.adblock.detect.js
@@ -1,13 +1,26 @@
 (function($){
 
 	/**
+	 * return check if this by adblock hide
+	 *
+	 * @example jQuery('.inline-ad').adblocked()
+	 * @return bool
+	 **/
+	$.fn.adblocked = function(){
+		return _ishide(this);
+	};
+
+	/**
 	 * get all by adblock hide elem
 	 *
+	 * @example jQuery('*').filter_adblocked()
 	 * @return [div.inline-ad, div#AdSense1.widget]
 	 **/
-	jQuery('*').filter(function(){
-		return _ishide(this);
-	});
+	$.fn.filter_adblocked = function(){
+		return $(this).filter(function(){
+			return $(this).adblocked();
+		});
+	};
 
 	function _ishide(elem) {
 		return jQuery(elem).css('-moz-binding').match(/url\s*\(\s*[\"\']?(about:abp-elemhidehit|chrome:\/\/global\/content\/bindings\/general\.xml#foobarbazdummy)/i);
