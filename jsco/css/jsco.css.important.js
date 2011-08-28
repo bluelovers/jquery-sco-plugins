aa73aefea45ac79fbefc886622d9a721049030b1
bluelovers <codelovers@users.sourceforge.net>
2011-08-29 03:08:49 +0800

    jQuery.rcamelCase

--- jsco/css/jsco.css.important.js
+++ jsco/css/jsco.css.important.js
@@ -3,5 +3,20 @@
  * @copyright 2011
  */
 (function($){
+	var
+		rrdashAlpha = /([A-Z]|[0-9])/ig,
+		rrmsPrefix = /^ms-/,
 
+		rfcamelCase = function( all, letter ) {
+			return ( "-" + letter ).toLowerCase();
+		},
+
+		undefined
+	;
+
+	jQuery.extend({
+		rcamelCase: function( string ) {
+			return string.replace( rrmsPrefix, "-ms" ).replace( rrdashAlpha, rfcamelCase );
+		},
+	});
 })(jQuery);
