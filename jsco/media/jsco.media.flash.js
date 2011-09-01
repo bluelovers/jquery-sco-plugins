70488b3e47766b3bc7cb1ea27e863efc028a4eb0
bluelovers <codelovers@users.sourceforge.net>
2011-09-01 23:03:49 +0800

    .

--- jsco/media/jsco.media.flash.js
+++ jsco/media/jsco.media.flash.js
@@ -9,21 +9,98 @@
 	$.scoEmbed.extend({
 		defaults : {
 			params : {
+				classid : 'clsid:d27cdb6e-ae6d-11cf-96b8-444553540000',
+				type : 'application/x-shockwave-flash',
 
+				codebase : 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0',
+				pluginspage : 'http://www.macromedia.com/go/getflashplayer',
 			},
 			object : {},
 			embed : {},
 		},
-		prop : {
+		defaults_empty : {
 			params : {},
 			object : {},
 			embed : {},
 		},
 		getArgs : function (options) {
 			var agv = $.extend(true, {}, $.scoEmbed.defaults.params, options);
+			var attr = $.extend(true, {}, $.scoEmbed.defaults_empty);
 
 			for(var _k in agv){
 				var _v = agv[_k];
+
+				switch (_k) {
+					case "classid":
+						attr.object[_k] = _v;
+						break;
+					case "pluginspage":
+						attr.embed[_k] = _v;
+						break;
+					case "src":
+						attr.object["movie"] = _v;
+						attr.embed[_k] = _v;
+						break;
+					case "codebase":
+						attr.object[_k] = _v;
+						break;
+					case "onafterupdate":
+					case "onbeforeupdate":
+					case "onblur":
+					case "oncellchange":
+					case "onclick":
+					case "ondblclick":
+					case "ondrag":
+					case "ondragend":
+					case "ondragenter":
+					case "ondragleave":
+					case "ondragover":
+					case "ondrop":
+					case "onfinish":
+					case "onfocus":
+					case "onhelp":
+					case "onmousedown":
+					case "onmouseup":
+					case "onmouseover":
+					case "onmousemove":
+					case "onmouseout":
+					case "onkeypress":
+					case "onkeydown":
+					case "onkeyup":
+					case "onload":
+					case "onlosecapture":
+					case "onpropertychange":
+					case "onreadystatechange":
+					case "onrowsdelete":
+					case "onrowenter":
+					case "onrowexit":
+					case "onrowsinserted":
+					case "onstart":
+					case "onscroll":
+					case "onbeforeeditfocus":
+					case "onactivate":
+					case "onbeforedeactivate":
+					case "ondeactivate":
+					case "type":
+					case "id":
+						attr.object[_k] = _v;
+						break;
+					case "width":
+					case "height":
+					case "align":
+					case "vspace":
+					case "hspace":
+					case "class":
+					case "title":
+					case "accesskey":
+					case "name":
+					case "tabindex":
+						attr.embed[_k] = attr.object[_k] = _v;
+						break;
+					default:
+						attr.embed[_k] = _v;
+						break;
+				};
 
 				agv[_k] = _v;
 			}
